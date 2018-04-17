80a6a1aae1fc6c5777816e7c11149187781af5b7
qingming <393974172.com>
2018-04-17 09:35:31 +0800

    第一次更新测试,加了"git更新测试"

--- src/test/git.java
+++ src/test/git.java
@@ -3,6 +3,7 @@ package test;
 public class git {
     public static void main(String[] args) {
         System.out.println("git上传下载测试");
-        System.out.println("git更新测试");
+        System.out.println("git更新测试/第一次");
+
     }
 }

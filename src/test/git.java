e796ed4583fc5a5b7f2af6f6971641d9e2f85678
AllenForIt <644625569@qq.com>
2018-04-17 12:03:52 +0800

    zzn  12.04

--- src/test/git.java
+++ src/test/git.java
@@ -9,9 +9,9 @@ public class git {
         System.out.print("老李也亮了");
         System.out.println("老李第二次上传使");
 
-        System.out.println （“xxxxxxxxx” ）;  
+        System.out.println("123");
         System.out.println("ZZN2222");
-        System.out.println("ZZN更改第N次");
+        System.out.println("ZZN更改第N次   12:03");
         System.out.println("老鸨第一次上传");
         System.out.println("小宝哥第那次上传");
     }

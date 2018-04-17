2a4b65ef71f020dd33536e2bd07e00ca06c8756f
bboya11en4 <644625569@qq.com>
2018-04-17 11:33:17 +0800

    zzn  11.32

--- src/test/git.java
+++ src/test/git.java
@@ -6,10 +6,10 @@ public class git {
         System.out.println("git更新测试/第一次");
         System.out.println("老周行不行");
         System.out.println("老周不行了");
-        System.out.print("老李也亮了");;
+        System.out.print("老李也亮了");
         System.out.println("老李第二次上传使");
         System.out.println("ZZN2222");
-        System.out.println("ZZN");
+        System.out.println("ZZN更改第N次");
         System.out.println("老鸨第一次上传");
     }
 }
